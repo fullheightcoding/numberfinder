81c92b5843add15644fe7345773a8329dd03ef34
fullheightcoding <mark.dodgson.developer@gmail.com>
2022-07-07 15:11:16 +1000

    update reactjs

--- reactjs-numberfinder-client/src/App.js
+++ reactjs-numberfinder-client/src/App.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import { nanoid } from 'nanoid';
-import Number from './components/Number';
-import Home from './Pages/Home'
-import NumberFinder from './Pages/NumberFinder';
+import './App.css';
+import { Routes, Route, Link, NavLink, BrowserRouter } from 'react-router-dom';
+import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
-import Header from './components/Header';
-import './App.css';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-import ErrorPage from './ErrorPage';
+import ErrorPage from './Pages/ErrorPage';
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Header/>
+    <>
+      <BrowserRouter>
 
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/numberfinder' element={<NumberFinder/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='*' element={<ErrorPage/>}/>
-      </Routes>
-    </BrowserRouter>
+        <nav>
+          <Link to="/" className="nav-item">Home</Link>
+          <Link to="/about" className="nav-item">About</Link>
+          <Link to="/contact" className="nav-item">Contact</Link>
+        </nav>
+
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='*' element={<ErrorPage/>}/>
+        </Routes> 
+      </BrowserRouter>
+
+    </>
   );
 }
 
